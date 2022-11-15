842781c91f13d5c49e79cea03ef1210c3dfdee6f
smabdulmueid97 <73439037+smabdulmueid97@users.noreply.github.com>
2022-11-15 23:12:06 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main() {
+    int i;
+    printf("Number 1 to 10:\n");
+    printf("Row 1: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 2: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 3: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 4: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 4: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 5: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 6: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 7: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 8: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 9: ");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    printf("\n");
+    printf("Row 10:");
+    for (i = 1; i < 11; ++i) {printf("%d", i);}
+    return 0;
+}
